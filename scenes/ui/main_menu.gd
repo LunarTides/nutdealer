1aab8b75c13388747ffa78ef380c3c9656d1dfa8
LunarTides <lunar@lunartides.dev>
2026-07-11 09:14:31 +0200

    Simplify main menu title pulse

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -14,7 +14,6 @@ var old_grid_position: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	title_label.get_node(^"AnimationPlayer").play(&"title")
 	init_grid()
 
 
@@ -40,25 +39,6 @@ func _on_play_button_pressed() -> void:
 
 func _on_create_button_pressed() -> void:
 	get_tree().change_scene_to_packed(CREATOR_DARK_WORLD_UI)
-
-func title_pulse() -> void:
-	if is_instance_valid(title_pulse_label):
-		title_pulse_label.queue_free()
-	
-	title_pulse_label = title_label.duplicate()
-	title_pulse_label.top_level = true
-	title_label.get_parent().add_child.call_deferred(title_pulse_label)
-	title_pulse_label.global_position = title_label.global_position
-	
-	# Waiting a single frame for some reason makes the animation smoother.
-	await get_tree().process_frame
-	
-	var animation_player: AnimationPlayer = title_pulse_label.get_node(^"AnimationPlayer")
-	animation_player.play(&"title_pulse")
-	animation_player.seek(title_label.get_node(^"AnimationPlayer").current_animation_position, true, true)
-
-func title_pulse_delete() -> void:
-	title_pulse_label.queue_free()
 
 func init_grid() -> void:
 	# Move the grid a little left and make it a little bigger.
