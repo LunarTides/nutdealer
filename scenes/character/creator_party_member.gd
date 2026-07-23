1ca859709696868d0be340cc63ea39b08749857a
LunarTides <lunar@lunartides.dev>
2026-07-24 00:19:42 +0200

    Add creator party member manager for better management

--- scenes/character/creator_party_member.gd
+++ scenes/character/creator_party_member.gd
@@ -28,7 +28,6 @@ func _ready() -> void:
 	position.y = Global.screen_size.y - 32 - 6
 	position.x = randf_range(32, Global.screen_size.x - 32)
 	create_state_loop()
-	create_dialogue_loop()
 
 
 # Called every physics frame. 'delta' is the elapsed time since the previous frame.
@@ -63,14 +62,6 @@ func create_state_loop() -> void:
 			await target_reached
 			await get_tree().create_timer(randf_range(1, 20)).timeout
 
-func create_dialogue_loop() -> void:
-	while true:
-		while state == State.Moving:
-			await get_tree().create_timer(randf_range(1, 30)).timeout
-			
-			if not in_dialogue:
-				do_random_dialogue()
-
 func pick_spot_to_move() -> void:
 	target = Vector2(randf_range(32, Global.screen_size.x - 32), position.y)
 
@@ -97,70 +88,3 @@ func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			if event.is_released():
 				click()
-
-func handle_dialogue_string(dialogue: PackedStringArray) -> void:
-	# Empty line.
-	print()
-	in_dialogue = true
-	
-	for line: String in dialogue:
-		var effect: String = line.split("]")[0].lstrip("[")
-		var effect_args: String
-		
-		var equal_split: PackedStringArray = effect.split("=")
-		if equal_split.size() >= 2:
-			effect_args = equal_split[1]
-			effect = equal_split[0]
-		
-		var text: String = line.split("]")[1]
-		
-		if effect == "wait":
-			var milli: float = effect_args.to_float()
-			await get_tree().create_timer(milli / 1000).timeout
-			continue
-		
-		if text:
-			if effect.to_lower() == "kris":
-				say(text, CharacterName.Kris)
-			elif effect.to_lower() == "susie":
-				say(text, CharacterName.Susie)
-			elif effect.to_lower() == "ralsei":
-				say(text, CharacterName.Ralsei)
-	
-	in_dialogue = false
-
-func do_random_dialogue() -> void:
-	var rand: int = randi_range(0, 1)
-	var dialogue: Dictionary[CharacterName, PackedStringArray]
-	
-	if rand == 0:
-		# Nothing
-		return
-	elif rand == 1:
-		dialogue = {
-			# let me have fun, okay >:(
-			CharacterName.Ralsei: [
-				"[ralsei]I wonder if this is grid is infinite...",
-				"[wait=3000]",
-				"[susie]Dude, what are you talking about?",
-			],
-			CharacterName.Susie: [
-				"[susie]So, whose nuts do you think they're dealing?",
-				"[wait=2000]",
-				"[ralsei]I think the nuts are supposed to be dark worlds?",
-				"[wait=2000]",
-				"[susie]Wait, they're creating dark worlds?",
-				"[kris]*perks up*",
-				"[wait=1000]",
-				"[susie]Isn't that, y'know, bad?",
-				"[wait=2000]",
-				"[ralsei]Um, well, yes.",
-				"[wait=3000]",
-				"[susie]Damn...",
-			]
-		}
-	
-	if not dialogue.has(character_name):
-		return
-	
-	handle_dialogue_string(dialogue[character_name])
---
+++ scenes/ui/creator/party_member_manager.gd
@@ -0,0 +1,77 @@
+extends Node
+
+var in_dialogue: bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	create_dialogue_loop()
+
+func create_dialogue_loop() -> void:
+	while true:
+		await get_tree().create_timer(randf_range(1, 30)).timeout
+		
+		if not in_dialogue:
+			do_random_dialogue()
+
+func say(text: String, speaker: String) -> void:
+	for party_member: CreatorPartyMember in get_children():
+		if party_member.name.to_lower() == speaker.to_lower():
+			party_member.say(text)
+
+func handle_dialogue_string(dialogue: PackedStringArray) -> void:
+	# Empty line.
+	print()
+	in_dialogue = true
+	
+	for line: String in dialogue:
+		var effect: String = line.split("]")[0].lstrip("[")
+		var effect_args: String
+		
+		var equal_split: PackedStringArray = effect.split("=")
+		if equal_split.size() >= 2:
+			effect_args = equal_split[1]
+			effect = equal_split[0]
+		
+		var text: String = line.split("]")[1]
+		
+		if effect == "wait":
+			var milli: float = effect_args.to_float()
+			await get_tree().create_timer(milli / 1000).timeout
+			continue
+		
+		if text:
+			say(text, effect)
+	
+	in_dialogue = false
+
+func do_random_dialogue() -> void:
+	var rand: int = randi_range(0, 2)
+	var dialogue: PackedStringArray
+	
+	if rand == 0:
+		# Nothing
+		return
+	elif rand == 1:
+		dialogue = [
+			"[ralsei]I wonder if this is grid is infinite...",
+			"[wait=3000]",
+			"[susie]Dude, what are you talking about?",
+		]
+	elif rand == 2:
+		dialogue = [
+			# let me have fun, okay >:(
+			"[susie]So, whose nuts do you think they're dealing?",
+			"[wait=2000]",
+			"[ralsei]I think the nuts are supposed to be dark worlds?",
+			"[wait=2000]",
+			"[susie]Wait, they're creating dark worlds?",
+			"[kris]*perks up*",
+			"[wait=1000]",
+			"[susie]Isn't that, y'know, bad?",
+			"[wait=2000]",
+			"[ralsei]Um, well, yes.",
+			"[wait=3000]",
+			"[susie]Damn...",
+		]
+	
+	handle_dialogue_string(dialogue)
