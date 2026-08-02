ca529ff80de8db50ffe6d3ab8453f8000fdd2302
LunarTides <lunar@lunartides.dev>
2026-08-02 21:25:00 +0200

    Draw room transition lines when selecting a tile

--- creator/select_outline.gd
+++ creator/select_outline.gd
@@ -43,4 +43,54 @@ func _draw() -> void:
 		if not tile.enabled:
 			continue
 		
-		draw_circle(tile.position + Vector2(32, 32), 8, color, false, 3)
+		var tile_center_position: Vector2 = tile.position + Vector2(32, 32)
+		draw_circle(tile_center_position, 8, color, false, 3)
+		
+		# Draw room transition stuff.
+		if is_instance_valid(tile.room_transition):
+			var target_position: Vector2 = Global.coords_to_position(tile.room_transition.coords)
+			var target_center_position: Vector2 = target_position + Vector2(32, 32)
+			
+			draw_dashed_line(
+				tile_center_position,
+				target_center_position,
+				color.darkened(0.2),
+				2.0,
+				4.0,
+			)
+			
+			var arc_center: Vector2 = Vector2(
+				(target_center_position.x + tile_center_position.x) / 2,
+				(target_center_position.y + tile_center_position.y) / 2,
+			)
+			var radius: float = tile_center_position.distance_to(target_center_position) / 2
+			var start_angle: float = (target_center_position - tile_center_position).angle()
+			var end_angle: float = (tile_center_position - target_center_position).angle()
+			if end_angle < 0:
+				end_angle += TAU
+			draw_arc(
+				arc_center,
+				radius,
+				start_angle,
+				end_angle,
+				16,
+				color.darkened(0.2),
+				2,
+			)
+			
+			draw_multiline([
+				target_position + Vector2(16, 16),
+				target_position + Vector2(48, 48),
+				target_position + Vector2(48, 16),
+				target_position + Vector2(16, 48),
+			], color, 3)
+			
+			var font: Font = preload("res://assets/fonts/main.ttf")
+			draw_string(
+				font,
+				target_position,
+				"Target",
+				HORIZONTAL_ALIGNMENT_CENTER,
+				-1,
+				18
+			)
